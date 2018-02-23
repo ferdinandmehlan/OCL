03de6a7e3445bb06f294b460aaae8683c1757daf
ferdinandmehlan <ferdinand.mehlan@rwth-aachen.de>
2018-02-23 13:10:27 +0100

    add cd output js

--- resources/js/plantUML.js
+++ resources/js/plantUML.js
@@ -3,7 +3,7 @@
     var cd = document.getElementById("cd");
 
     function onCDOutputClick(event) {
-        cd.src = "http://www.plantuml.com/plantuml/proxy?src=https://ferdinandmehlan.github.io/OCL/resources/txt/plantUML.txt";
+        cd.src = "http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/ferdinandmehlan/OCL/gh-pages/resources/txt/plantUML.txt";
     }
 
     cdOutput.addEventListener("click", onCDOutputClick);
