e2be0cdf2b45180db35176afa74820b888f43033
ferdinandmehlan <ferdinand.mehlan@rwth-aachen.de>
2018-02-24 19:48:42 +0100

    ry calling static function

--- resources/js/plantUML.js
+++ resources/js/plantUML.js
@@ -3,18 +3,21 @@
 
     function onThen() {
         // Send translated file to plantuml
-        cdOutput.src = "";
+        // cdOutput.src = "";
     }
 
     function onCD4AReadFile4Print(error, cdString) {
         if(error)
             console.error("An error occurred while reading the CD4A file for visualizing!");
-        else
-            cheerpjRunMain("ocl.cli.OCLCDTool", "/app/OCL/ocl-1.2.2-cli.jar", "-printSrc", cdString, "-printTgt", "resources/txt/plantUML.txt").then(onThen);
+        else {
+            var returnVal = cjCall("ocl.cli.OCLCDTool", "printCD2PlantUML", cdString);
+            var plantUMLString = cjStringJavatoJS(returnVal);
+        }
+            //cheerpjRunMain("ocl.cli.OCLCDTool", "/app/OCL/ocl-1.2.2-cli.jar", "-printSrc", cdString, "-printTgt", "resources/txt/plantUML.txt").then(onThen);
     }
 
     function onClick(event) {
-            CD4A.readFile(onCD4AReadFile4Print);
+        CD4A.readFile(onCD4AReadFile4Print);
     }
 
     //is already initialized in cheerpj.js
