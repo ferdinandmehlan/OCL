d3fada190424ea59afc11c58c811f96b22d991f3
ferdinandmehlan <ferdinand.mehlan@rwth-aachen.de>
2018-02-24 21:49:25 +0100

    try calling static function

--- resources/js/plantUML.js
+++ resources/js/plantUML.js
@@ -1,17 +1,17 @@
 (function() {
     var cdOutput = document.getElementById("cd-output");
+    var plantUMLString = "@startuml\n@enduml";
 
-    function onThen() {
+    function getPng() {
         // Send translated file to plantuml
-        // cdOutput.src = "";
+        cdOutput.src = "";
     }
 
     function onCD4AReadFile4Print(error, cdString) {
         if(error)
             console.error("An error occurred while reading the CD4A file for visualizing!");
         else {
-            var returnVal = cjCall("ocl.cli.OCLCDTool", "printCD2PlantUML", cdString);
-            var plantUMLString = cjStringJavatoJS(returnVal);
+            var returnVal = cjCall("ocl.cli.OCLCDTool", "printCD2PlantUML", cdString).then(plantUMLString = cjStringJavatoJS(returnVal)).then(getPng);
         }
     }
 
