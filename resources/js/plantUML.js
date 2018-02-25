cd584b6fab30827d73d19b0eaf2b2deedf62b12b
ferdinandmehlan <ferdinand.mehlan@rwth-aachen.de>
2018-02-25 15:18:56 +0100

    try new filewriter method

--- resources/js/plantUML.js
+++ resources/js/plantUML.js
@@ -17,9 +17,10 @@
             console.error("An error occurred while reading the CD4A file for visualizing!");
         else {
             // translate MC-CD to plantUML-CD
-            //cheerpjRunMain("ocl.cli.OCLCDTool", "/app/OCL/ocl-1.2.2-cli.jar", "-printSrc", cdString, "-printTgt", "resources/txt/plantUML.txt").then(getPng);
             var plantumlCjString = cjCall("ocl.cli.OCLCDTool", "printCD2PlantUML", cdString);
             var plantumlJsString = cjStringJavaToJs(plantumlCjString);
+            // or try and write to file
+            cheerpjRunMain("ocl.cli.OCLCDTool", "/app/OCL/ocl-1.2.2-cli.jar", "-printSrc", cdString, "-printTgt", "plantUML.txt").then(getPng);
         }
     }
 
