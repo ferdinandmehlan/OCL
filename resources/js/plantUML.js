2bee6acbbdcb885594a938deccbb0ff40ee3bff6
ferdinandmehlan <ferdinand.mehlan@rwth-aachen.de>
2018-02-25 17:41:53 +0100

    fix typo

--- resources/js/plantUML.js
+++ resources/js/plantUML.js
@@ -86,7 +86,7 @@
     }
 
     function done_deflating(e) {
-    	cdoutput.src = "http://www.plantuml.com/plantuml/img/"+encode64(e.data);
+    	cdOutput.src = "http://www.plantuml.com/plantuml/img/"+encode64(e.data);
     }
 
     function compress(s) {
