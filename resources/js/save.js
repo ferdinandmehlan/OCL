7428d7066be645b84b859d8adc407b77c92b565b
Michael von Wenckstern <30497492+vonwenckstern@users.noreply.github.com>
2018-02-27 01:36:13 +0100

    Update save.js

--- resources/js/save.js
+++ resources/js/save.js
@@ -1,18 +1,4 @@
 (function() {
-
-    // First, checks if it isn't implemented yet.
-    if (!String.prototype.format) {
-      String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) { 
-          return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-          ;
-        });
-      };
-    }
-
     var buttonSave = document.getElementById("button-save");
 
    function onClick(event) {
